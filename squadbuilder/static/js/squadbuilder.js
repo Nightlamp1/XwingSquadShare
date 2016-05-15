e363ef2c9b4b7c20e3328b983260fe62e6cd3e59
Rob <robert.britt2011@gmail.com>
2016-05-15 17:04:43 -0500

    Adding basic upgrade functionality Added some basic functionality including upgrade selection/cost modification based on selections.

--- squadbuilder/static/js/squadbuilder.js
+++ squadbuilder/static/js/squadbuilder.js
@@ -3,6 +3,7 @@ var cost = 0;
 //The following variables are defined in builder.html based on query data from views.py
 //var upgrades = {{ upgrades|safe }}; A dict of {pilot:upgradeArray}
 //var pilotCost = {{pilotCost|safe}}; A dict of {pilot:cost}
+//var upgradeCardList = {{cards|safe}}; A dict of Upgrade card/cost sorted by type
 
 
 $(document).ready(function() {
@@ -10,39 +11,83 @@ $(document).ready(function() {
 //  for initializing variables and finalizing the page after the client has 
 //	finished loading.
 
-$("#cost").text(cost +"/100");
-console.log("ready!!");
-$("#mytabs").click(function (e){
-	console.log("made it into mytabs");
-	$("input:checkbox").prop('checked', false);
-	$("#squad").empty();
-	cost = 0;
-	updateCostDisplay(cost);
-});
-$(".pilotCheckbox").change(function() {
-  // Attach an event to all objects with class pilotCheckbox change events
-  if(this.checked) {
-	var htmlString = "";
-	
-	for (i=0; i<upgrades[this.id].length; i++){
-	htmlString += (
-	"<span class='dropdown'>"+
-		"<button type='button' class='btn btn-default dropdown-toggle' id='test' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>"+ upgrades[this.id][i] + "</button>"+
-		"<ul class='dropdown-menu' aria-labelledby='test'><li>Test1</li><li>Test2</li></ul>"+
-	"</span>");
-	}
-	
-	$("#squad").append("<div class=" + this.id + ">" + '<img src="/static/img/Pilots/' + this.id + '.jpg" height=209px width=150px>' + htmlString + "</div>");
-	cost+=pilotCost[this.id];
-	updateCostDisplay(cost);
-	
+	$("#cost").text(cost +"/100");
+	console.log("ready!!");
+	$("#mytabs").click(function (e){
+		console.log("made it into mytabs");
+		$("input:checkbox").prop('checked', false);
+		$("#squad").empty();
+		cost = 0;
+		updateCostDisplay(cost);
+	});
 
-  } else {
-	$("div." +this.id).last().remove();
-	cost-=pilotCost[this.id];
-	updateCostDisplay(cost);
-  }
-});
+	$(".pilotCheckbox").change(function() {
+	  // Attach an event to all objects with class pilotCheckbox change events
+	  if(this.checked) {
+		var htmlString = "";
+		
+		for (i=0; i<upgrades[this.id].length; i++){
+			htmlString += (
+			"<span class='dropdown'>"+
+				"<button type='button' class='btn btn-default dropdown-toggle' id='test' data-toggle='dropdown' aria-haspopup='true' aria-expanded='true'>"+ upgrades[this.id][i] + "</button>"+
+				"<ul class='dropdown-menu' aria-labelledby='test'>");
+		
+			upgradeCardArray=Object.keys(upgradeCardList[upgrades[this.id][i]]);
+			for (j=0; j<upgradeCardArray.length; j++){
+				htmlString+= ("<li><a class='upgrade' id=" + upgrades[this.id][i] + ">" + upgradeCardArray[j] + "</a></li>");
+				
+			}
+			htmlString+=("<li><a class='upgrade' id=" + upgrades[this.id][i] + ">None</a></li></ul>"+"</span>");
+		
+		}
+		
+		$("#squad").append("<div class=" + this.id + ">" + '<img src="/static/img/Pilots/' + this.id + '.jpg" height=259px width=200px>' + htmlString + "</div>");
+		cost+=pilotCost[this.id];
+		updateCostDisplay(cost);
+		
+
+	  } else {
+		$("div." +this.id).last().remove();
+		cost-=pilotCost[this.id];
+		updateCostDisplay(cost);
+	  }
+	});
+
+	$(document.body).on('click','.upgrade',function(){
+		console.log("Upgrade was clicked");
+		
+		var selectedUpgrade=$(this).text().replace(/\s/g,"");
+		var pilot=$(this).closest('div').attr('class');
+		var upgradeType = $(this).attr('id');
+		var upgradeCost=0;
+		
+		if(selectedUpgrade=="None"){
+			console.log("made it into none");
+			upgradeCost=$("."+upgradeType+pilot).data("cost");
+			$("."+upgradeType+pilot).remove();
+			updateCostDisplay(cost-=upgradeCost);
+			
+		}
+		else if($("."+upgradeType+pilot).length==0){
+			upgradeCost=upgradeCardList[upgradeType][$(this).text()]['cost'];
+			$("."+pilot).append('<span class=' + upgradeType + pilot + '> <img id=' + selectedUpgrade +' src="/static/img/Upgrades/' +selectedUpgrade+'.jpg" height=209px width=150px></span>');
+			$("."+upgradeType+pilot).data("cost", upgradeCost);
+			updateCostDisplay(cost+=upgradeCost);
+		}
+		else{
+			upgradeCost=$("."+upgradeType+pilot).data("cost");
+			updateCostDisplay(cost-=upgradeCost);
+			$("."+upgradeType+pilot).remove();
+			
+			upgradeCost=upgradeCardList[upgradeType][$(this).text()]['cost'];
+			$("."+pilot).append('<span class=' + upgradeType + pilot + '> <img id=' + selectedUpgrade +' src="/static/img/Upgrades/' +selectedUpgrade+'.jpg" height=209px width=150px></span>');
+			$("."+upgradeType+pilot).data("cost", upgradeCost);
+			updateCostDisplay(cost+=upgradeCost);
+		}
+		
+		
+		console.log(upgradeCost);
+	});
 });
 
 function updateCostDisplay(newvalue){
